53b3d92517148ad04918651ed06a251205acf355
Javier Ribó <elribonazo@gmail.com>
2025-09-11 22:44:42 +0200

    fix: temp disable models Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -7,8 +7,8 @@ import { HuggingFaceONNX } from './llm/HuggingFaceONNX';
 
 export enum HuggingFaceONNXModels {
   Llama32_1B = "onnx-community/Llama-3.2-1B-Instruct-q4f16",
-  LMF2_350M = "onnx-community/LFM2-350M-ONNX",
-  LMF2_700M = "onnx-community/LFM2-700M-ONNX",
+  // LMF2_350M = "onnx-community/LFM2-350M-ONNX",
+  // LMF2_700M = "onnx-community/LFM2-700M-ONNX",
   LMF2_1_2B = "onnx-community/LFM2-1.2B-ONNX",
   QWEN_1 = "onnx-community/Qwen3-0.6B-ONNX",
 }
