c20359b5f32b64ed7de352ddca86f9a97d6ecbbc
Javier Ribó <elribonazo@gmail.com>
2025-09-14 11:29:59 +0200

    fix: remove debugger Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- apps/uaito/src/redux/userSlice.tsx
+++ apps/uaito/src/redux/userSlice.tsx
@@ -113,7 +113,6 @@ const userSlice = createSlice({
     pushChatMessage: (state, action: PayloadAction<PushChatMessage>) => {
       const { chatMessage: { message } } = action.payload;
       if (message.type === "error") {
-        debugger;
         toast(
           <div>
             <p>{message.content[0].type === "error" ?
