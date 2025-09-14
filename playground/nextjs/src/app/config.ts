8798853bacd7689bbe58fec6d222e919d326cae0
Javier Ribó <elribonazo@gmail.com>
2025-09-14 11:32:25 +0200

    fix: simplify playground Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- playground/nextjs/src/app/config.ts
+++
@@ -1,4 +0,0 @@
-
-type DType = "auto" | "fp32" | "fp16" | "q8" | "int8" | "uint8" | "q4" | "bnb4" | "q4f16" | Record<string, "auto" | "fp32" | "fp16" | "q8" | "int8" | "uint8" | "q4" | "bnb4" | "q4f16"> | undefined
-
-
